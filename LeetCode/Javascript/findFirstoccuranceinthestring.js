9daed47ed158f4d60e0b201de23cd94fcb882629
srajalingam <rajalingam299@gmail.com>
2026-07-01 01:38:41 +0530

    findFirstOccurrenceInTheStringOptimization JS

---
+++ LeetCode/Javascript/findFirstoccuranceinthestring.js
@@ -0,0 +1,45 @@
+function findFirstoccuranceinthestring(haystack, needle){
+    for (let i = 0; i <= haystack.length; i++) {
+
+        let found = true;
+
+        for (let j = 0; j < needle.length; j++) {
+            if (haystack[i + j] !== needle[j]) {
+                found = false;
+                break;
+            }
+        }
+
+        if (found) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+
+console.log(findFirstoccuranceinthestring('rabia', 'ia'))
+
+
+findFirstoccuranceinthestringOptimization(haystack, needle){
+     for (let i = 0; i <= haystack.length - needle.length; i++) {
+
+        let found = true;
+
+        for (let j = 0; j < needle.length; j++) {
+            if (haystack[i + j] !== needle[j]) {
+                found = false;
+                break;
+            }
+        }
+
+        if (found) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+console.log(findFirstoccuranceinthestringOptimization('rabia', 'ia'))
