bc8b183d6c5ce172eb5ac91ae2670cc2f5592ec4
srajalingam <rajalingam299@gmail.com>
2026-07-12 17:45:18 +0530

    bredth first search JS solution

--- LeetCode/Javascript/BinarySearchTree/bstInsertSln.js
+++ LeetCode/Javascript/BinarySearchTree/bstInsertSln.js
@@ -56,6 +56,25 @@ class BST{
         if(!found) return undefined
         return current
     }
+    //breadth first search
+    BFS(){
+        var node=this.root;
+        var data=[];
+        var queue=[];
+        queue.push(node)
+
+        while(queue.length){
+            node=queue.shift();
+            data.push(node.value);
+            if(node.left) {
+                queue.push(node.left)
+            }
+            if(node.right){
+                queue.push(node.right)
+            }
+        }
+        return data;
+    }
 }
 
 var tree = new BST();
@@ -68,3 +87,4 @@ tree.insert(2)
 tree.insert(6)
 
 console.log(tree)
+console.log(tree.BFS())
